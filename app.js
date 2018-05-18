d427c1caf977249d102ae63bb36e099f14077edd
yaeger <mug4019@gmail.com>
2018-05-18 17:43:33 +0900

    make new chatroom function

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ var multer = require('multer');
 var upload = multer({dest: 'public/'});
 var mysql = require('mysql');
 
-
 var app = express();
+
+var auth = {
+    user_id: null,
+    user_nickname: null,
+    user_profile: null,
+}
 
 var conn = mysql.createConnection({
     host     : 'localhost',
@@ -20,18 +25,14 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }))
 
-var auth = {
-    user_id: null,
-    user_nickname: null,
-    user_profile: null,
-}
+
 
 
 app.get('/', function (req, res) {
     
     if(auth.user_id){
         console.log('authed');
-        var sql = `SELECT * FROM chatroom;`;
+        var sql = `SELECT chatroom_id ,chatroom_name, hash_tag, (SELECT COUNT(participate.user_id)  FROM participate WHERE participate.chatroom_id = chatroom.chatroom_id AND participate.banish = 0) AS person FROM chatroom`;
         conn.query(sql, (err, chatrooms, fields)=>{
         if(err){
             console.log("error___");
@@ -39,8 +40,18 @@ app.get('/', function (req, res) {
             return;
         } 
         else if (chatrooms.length > 0) {
-            console.log(chatrooms);
-            res.render('home', {chatrooms:chatrooms, auth:auth} );
+            var sql = `SELECT chatroom_name FROM chatroom JOIN participate ON participate.chatroom_id = chatroom.chatroom_id WHERE participate.user_id = ? AND banish = 0`;    
+            conn.query(sql, [auth.user_id], (err, participated, fields)=>{
+                if(err){
+                    console.error('error connecting: ' + err.stack);
+                    return;
+                }else if (participated.length > 0){
+                    res.render('home', {chatrooms:chatrooms,participated:participated, auth:auth} );
+                }else{
+                    res.render('home', {chatrooms:chatrooms,participated:'No participated Room', auth:auth} );
+                }
+            });
+            
         }else{
             console.log("What?");
             res.render('home', {chatrooms:null, auth:auth});
@@ -59,12 +70,37 @@ app.get('/signup', function (req, res) {
     res.render('signup');
 });
 
+app.get('/newchat', function (req, res) {
+    if(auth.user_id){
+        res.render('newchat');
+    }
+    else{
+        res.redirect('/');
+    }
+});
+
+app.post('/newchat' ,(req, res) => {
+    var room_name = req.body.room_name;
+    var hashtag = req.body.hash;
+    var room_type = req.body.room_type;
+    var host = auth.user_id;
+    
+    var sql = `INSERT INTO chatroom (chatroom_name, user_id, room_type, hash_tag) VALUES ( ?, ?, ?, ?);` ;
+    conn.query(sql, [room_name, host, room_type, hashtag], (err, users, fields)=>{
+        if(err){
+            console.error('error connecting: ' + err.stack);
+            return;
+        } else{
+            console.log("new-chat");
+            res.redirect('/');
+        }
+    });
+});
+
 app.post('/signup', upload.single('profile_img') ,(req, res) => {
-    console.log("post file");
     var id = req.body.id;
     var pw = req.body.pw;
     var nick = req.body.nick;
-    console.log(req.file);
     var url = req.file.filename;
     
     var sql = `INSERT INTO users (user_id, user_pw, user_nickname, user_profile) VALUES ( ?, ?, ?, ?);` ;
@@ -73,13 +109,13 @@ app.post('/signup', upload.single('profile_img') ,(req, res) => {
             console.error('error connecting: ' + err.stack);
             return;
         } else{
-            console.log("What?");
+            console.log(users);
             res.render('login', {signup:'Signup Success'});
         }
     });
 });
 
-app.post('/login_process', function(req, res){
+app.post('/login', function(req, res){
     var id = req.body.id;
     var pw = req.body.pw;
     var sql = `SELECT * FROM users WHERE user_id=?  AND user_pw=?;`;
@@ -90,7 +126,7 @@ app.post('/login_process', function(req, res){
             return;
         } 
         else if (users.length > 0) {
-            console.log("Working");
+            console.log("login successful");
             auth = {
                 user_id : users[0].user_id,
                 user_nickname : users[0].user_nickname,
@@ -98,11 +134,35 @@ app.post('/login_process', function(req, res){
             }
             res.render('loged', {auth:auth} );
         }else{
-            console.log("What?");
+            console.log("login fail");
             res.render('login', {failed:'Login failed'});
         }
     });
   });
+
+app.get('/logout', function (req, res) {
+    auth = {
+        user_id: null,
+        user_nickname: null,
+        user_profile: null,
+    }
+    res.redirect('/');
+});
+
+app.get('/join/:chatroom_id', function (req, res) {
+    var chatroom_id = req.params.chatroom_id;
+    var sql = `INSERT INTO participate (chatroom_id, user_id, banish, ptime) VALUES ( ?, ?, '0', CURRENT_TIMESTAMP);` 
+    conn.query(sql, [chatroom_id, auth.user_id], (err, participated, fields)=>{
+        if(err){
+            console.error('error connecting: ' + err.stack);
+            return;
+        }else{
+            console.log(participated);
+            res.redirect('/');
+        }
+    });
+});
+
 
 app.listen(3000, function(){
   console.log('connected :3000');
